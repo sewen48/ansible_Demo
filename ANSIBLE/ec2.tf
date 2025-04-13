20257c8af552a27052d9e6f8ca57b21f76fbf98b
sewen48 <gadelaja48@gmail.com>
2025-04-13 11:03:07 +0100

    update profile

--- ANSIBLE/ec2.tf
+++ ANSIBLE/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "yusuf"
+  profile = "ganiyy"
 }
 
 
@@ -79,7 +79,7 @@ data "aws_ami" "ubuntu" {
 
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
     }
 
     filter {
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.medium"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group2.id]
-  key_name               = "devopskeypair"
+  key_name               = "zeeKP"
   count                  = 4
 
   tags = {
